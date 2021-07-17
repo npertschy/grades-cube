0e88ecbbd2053c7f1e5e2ad93fe9101b0d5f9775
Nikolas Pertschy <nikolas.pertschy@googlemail.com>
2021-07-17 19:56:21 +0200

    added primeflex

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,16 @@ import 'primevue/resources/themes/saga-blue/theme.css' //theme
 import 'primevue/resources/primevue.min.css' //core css
 import 'primeicons/primeicons.css' //icons
 
+// import 'primeflex/src/_variables.scss';
+// import 'primeflex/src/_grid.scss';
+// import 'primeflex/src/_formlayout.scss';
+// import 'primeflex/src/_display.scss';
+// import 'primeflex/src/_text.scss';
+// import 'primeflex/src/flexbox/_flexbox.scss';
+// import 'primeflex/src/_spacing.scss';
+// import 'primeflex/src/_elevation.scss';
+import 'primeflex/primeflex.css';
+
 const app = createApp(App)
 app.use(PrimeVue)
 app.mount('#app')
