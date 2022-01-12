ec976a0753891cee0664d2c5c7a2db48b33e7120
Nikolas Pertschy <nikolas.pertschy@googlemail.com>
2022-01-12 10:17:19 +0100

    Updated settings and dependencies

--- jest.config.ts
+++ jest.config.ts
@@ -5,11 +5,12 @@ const config: Config.InitialOptions = {
   moduleFileExtensions: [...defaults.moduleFileExtensions, 'vue'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
-    '^.+\\.vue$': 'vue-jest'
+    '^.+\\.vue$': '@vue/vue3-jest'
   },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1'
-  }
+  },
+  testEnvironment: 'jsdom'
 }
 
 export default config
