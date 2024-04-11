a1df352bdd3b10d2947db874e541367cac2129d4
Nikolas Pertschy <npertschy@inmediasp.de>
2024-04-11 13:26:51 +0200

    Configured eslint and prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,22 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript'
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/vue3-recommended",
+    "@vue/eslint-config-typescript",
+    "prettier"
   ],
   overrides: [
     {
-      files: [
-        'e2e/**/*.{test,spec}.{js,ts,jsx,tsx}'
-      ],
-      'extends': [
-        'plugin:playwright/recommended'
-      ]
-    }
+      files: ["e2e/**/*.{test,spec}.{js,ts,jsx,tsx}"],
+      extends: ["plugin:playwright/recommended"],
+    },
   ],
+  parser: "vue-eslint-parser",
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: "latest",
+  },
+};
