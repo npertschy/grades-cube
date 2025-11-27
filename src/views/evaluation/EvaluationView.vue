e72323bdc38d8241333af316de653f4d789d8d55
Lina Unger <Lina@Mac.fritz.box>
2025-11-27 20:43:35 +0100

    Some dependency updates and updates to evaluation view

--- src/views/evaluation/EvaluationView.vue
+++ src/views/evaluation/EvaluationView.vue
@@ -175,45 +175,108 @@ const chartData = computed(() => {
   if (selectedColumn.value) {
     const performance = performances.value.find((performance) => performance.id === selectedColumn.value);
     if (performance) {
-      const labels =
-        performance.type === 0
-          ? ["++", "+", "0", "-", "--", "f"]
-          : Array.from({ length: 16 }, (_, i) => i.toString()).reverse();
-      const grades = students.value
-        .map((student) => student.grades.get(performance.performanceId!)?.value)
-        .filter((grade) => grade !== undefined)
-        .filter((grade) => grade !== null)
-        .filter((grade) => grade != "") as string[];
-      const histogram: number[] = [];
-      labels.forEach((label) => {
-        histogram.push(grades.filter((grade) => grade === label).length);
-      });
-
-      const average = grades.reduce((acc, grade) => acc + parseInt(grade), 0) / grades.length;
-
-      let label = `Notenverteilung für ${performance.title} und ${grades.length} bewertete Schüler`;
-
-      if (performance.type > 0) {
-        label += ` - (Durchschnitt: ${average.toFixed(2)})`;
+      if (performance.type === 0) {
+        return chartDataForOralPerformance(performance);
+      } else {
+        return chartDataForNonOralPerformance(performance);
       }
-
-      return {
-        labels,
-        datasets: [
-          {
-            label: label,
-            data: histogram,
-            borderColor: "rgba(8, 145, 178, 0.5)",
-            backgroundColor: "rgba(236, 254, 255, 1)",
-            borderWidth: 2,
-          },
-        ],
-      };
     }
   }
 
   return {};
 });
+
+function chartDataForOralPerformance(performance: Performance) {
+  const labels = ["++", "+", "0", "-", "--", "f"];
+  const grades = students.value
+    .map((student) => student.grades.get(performance.performanceId!)?.value)
+    .filter((grade) => grade !== undefined)
+    .filter((grade) => grade !== null)
+    .filter((grade) => grade != "") as string[];
+  const histogram: number[] = [];
+  labels.forEach((label) => {
+    histogram.push(grades.filter((grade) => grade === label).length);
+  });
+
+  const average = histogram.reduce((acc, grade, index) => acc + grade * (index + 1), 0) / grades.length;
+
+  let label = `Notenverteilung für ${performance.title} und ${grades.length} bewertete Schüler - Durchschnitt: ${labels[Math.floor(average) - 1]}`;
+
+  return {
+    labels,
+    datasets: [
+      {
+        label: label,
+        data: histogram,
+        borderColor: "rgba(8, 145, 178, 0.5)",
+        backgroundColor: "rgba(236, 254, 255, 1)",
+        borderWidth: 2,
+      },
+    ],
+  };
+}
+
+function chartDataForNonOralPerformance(performance: Performance) {
+  const labels = Array.from({ length: 16 }, (_, i) => i.toString()).reverse();
+  const grades = students.value
+    .map((student) => student.grades.get(performance.performanceId!)?.value)
+    .filter((grade) => grade !== undefined)
+    .filter((grade) => grade !== null)
+    .filter((grade) => grade != "") as string[];
+
+  const histogram: number[] = [];
+  labels.forEach((label) => {
+    histogram.push(grades.filter((grade) => grade === label).length);
+  });
+
+  const groupedLabels = ["1", "2", "3", "4", "5", "6"];
+  const groupedHistogram: number[] = [0, 0, 0, 0, 0, 0];
+  grades.forEach((grade) => {
+    if (grade > "12") {
+      groupedHistogram[0] += 1;
+    } else if (grade <= "12" && grade > "9") {
+      groupedHistogram[1] += 1;
+    } else if (grade <= "9" && grade > "6") {
+      groupedHistogram[2] += 1;
+    } else if (grade <= "6" && grade > "3") {
+      groupedHistogram[3] += 1;
+    } else if (grade <= "3" && grade > "0") {
+      groupedHistogram[4] += 1;
+    } else {
+      groupedHistogram[5] += 1;
+    }
+  });
+
+  const average = histogram.reduce((acc, grade, index) => acc + grade * (16 - index), 0) / grades.length;
+  const groupedAverage = groupedHistogram.reduce((acc, grade, index) => acc + (index + 1) * grade, 0) / grades.length;
+
+  let label = `Notenverteilung für ${performance.title} und ${grades.length} bewertete Schüler`;
+  label += ` - (Durchschnitt: ${average.toFixed(2)})`;
+
+  let groupedLabel = `Notenverteilung für ${performance.title} und ${grades.length} bewertete Schüler`;
+  groupedLabel += ` - (Durchschnitt: ${groupedAverage.toFixed(2)})`;
+
+  return {
+    labels,
+    datasets: [
+      {
+        xAxisID: "bla",
+        label: label,
+        data: histogram,
+        borderColor: "rgba(8, 145, 178, 0.5)",
+        backgroundColor: "rgba(236, 254, 255, 1)",
+        borderWidth: 2,
+      },
+      // {
+      //   label: groupedLabel,
+      //   data: groupedHistogram,
+      //   borderColor: "rgba(7, 79, 79, 0.5)",
+      //   backgroundColor: "rgba(179, 183, 112, 1)",
+      //   borderWidth: 2,
+      // },
+    ],
+  };
+}
 
 async function handleGradeChanged(event: DataTableCellEditCompleteEvent) {
   const performanceId = event.field;
@@ -346,6 +409,7 @@ function allowedGradesByPerformance(performance: Performance) {
           scroll-height="80vh"
           row-hover
           edit-mode="cell"
+          style="overflow-x: scroll"
           @cell-edit-complete="handleGradeChanged"
         >
           <template #header>
@@ -425,7 +489,7 @@ function allowedGradesByPerformance(performance: Performance) {
           <p-column
             header="#"
             frozen
-            style="width: 2rem"
+            style="min-width: 2rem"
           >
             <template #body="headerProps">
               {{ headerProps.index + 1 }}
@@ -434,7 +498,7 @@ function allowedGradesByPerformance(performance: Performance) {
           <p-column
             header="Name"
             frozen
-            style="width: 14rem"
+            style="min-width: 14rem"
           >
             <template #body="slotProps">
               {{ slotProps.data.student.firstName }} {{ slotProps.data.student.lastName }}
@@ -445,7 +509,7 @@ function allowedGradesByPerformance(performance: Performance) {
             :key="performance.id"
             :field="performance.performanceId"
             :style="backgroundColorByType(performance.type)"
-            style="width: fit-content; min-width: 15px; max-width: 30px"
+            style="width: fit-content; min-width: 2rem"
           >
             <template #header>
               <span
